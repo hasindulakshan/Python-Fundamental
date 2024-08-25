2b8964850a2ae90e0da7850a84159ea16e4654c0
Hasindu Lakshan <116471812+hasindulakshan@users.noreply.github.com>
2024-08-25 09:10:54 +0530

    Slicing

--- 1. Basic Data Structures/7. Slicing.py
+++ 1. Basic Data Structures/7. Slicing.py
@@ -1,8 +1,8 @@
-# Slicing in python list is a way to get a subset of elements from a list. it denote by [start:end]
+# Slicing in Python list is a way to get a subset of elements from a list. it denote by [start:end]
 list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-list2 = list1[2:5]  # Get the elements from index 2 to 4. this is called slicing.
+list2 = list1[2:5]  # Get the elements from index 2 to 4. This is called slicing.
 print(list2)  # Output: [3, 4, 5]
 
-# we can use [:-1] to get all elements without the last element. [-1] to get the last element.
+# We can use [:-1] to get all elements without the last element. [-1] to get the last element.
 
-# we can always slice strings, tuples, and sets in the same way as lists. but we cannot slice dictionaries. because dictionaries are unordered.
+# We can always slice strings, tuples, and sets in the same way as lists. but we cannot slice dictionaries. because dictionaries are unordered.
