896de4cfe7e12dd7f589274eaa541b50ae562403
Hasindu Lakshan <hasindulakshan0040@gmail.com.com>
2024-08-26 16:29:54 +0530

    file i/o

---
+++ File input output/writeFile.py
@@ -0,0 +1,10 @@
+# In append file, the contents are added to the end of the file.
+
+myFile = open("./File input output/dataFile2.txt", "w") # Open the file in write mode
+
+myFile.write("Opening files: Use open() with the appropriate mode ('r', 'w', 'a', etc.)." + "\n")
+myFile.write("Reading files: Use read(), readline(), or readlines() methods." + "\n") 
+myFile.write("Writing files: Use write() or writelines() methods." + "\n") 
+myFile.write("Closing files: Always close files after use, preferably with a with statement." + "\n") 
+
+myFile.close() # Close the file
