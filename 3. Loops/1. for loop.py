2b8920997f771f63d2f2a020e21969a9c7711642
Hasindu Lakshan <hasindulakshan0040@gmail.com.com>
2024-08-30 22:11:33 +0530

    break & continue

--- 3. Loops/1. for loop.py
+++ 3. Loops/1. for loop.py
@@ -1,16 +1,18 @@
 # For Loop in Python is used to iterate over a sequence (list, tuple, string) or other iterable objects. or else we can call foreach loop.
 
-x = [1, 2, 3, 4, 5] # List
+x = [1, 2, 3, 4, 5]  # List
 
-index = 0 # We use the index to get the value of the list and print the value through an index of the list.
+# We use the index to get the value of the list and print the value through an index of the list.
+index = 0
 for item in x:
     y = x[index]
     print(y)
     index += 1
-    
+
 # We can also use this method to get the value of the list.
 
-for index, item in enumerate(x): # enumerate() function is used to get the index and value of the list.
+# enumerate() function is used to get the index and value of the list.
+for index, item in enumerate(x):
     print(index, item)
 
 # if we need to run a loop for a specific number of times then we can use the range() function.
@@ -19,20 +21,21 @@ for item in range(0, 10):
     print(type)
 
 
-## We also can iterate the dictionary using a for loop.
+# We also can iterate the dictionary using a for loop.
 
 myDictionary = {
     "Age": 25,
-    "Name": "Hasindu", 
+    "Name": "Hasindu",
     "Country": "Sri Lanka",
     "City": "Colombo"
 }
 
-for key, value in myDictionary.items(): 
-    print(key, value) # items() function is used to get the key and value of the dictionary.
+for key, value in myDictionary.items():
+    # items() function is used to get the key and value of the dictionary.
+    print(key, value)
 
 
-## iterate set using for loop
+# iterate set using for loop
 
 mySet = {
     1,
@@ -43,12 +46,14 @@ mySet = {
 }
 
 for item in mySet:
-    print(item) # set is an unordered collection of items, so the output may not be in the order of the input.
+    # set is an unordered collection of items, so the output may not be in the order of the input.
+    print(item)
 
 
-## iterate tuple using for loop
+# iterate tuple using for loop
 
-myTuple = ("Hasindu", 2, 3, "BSE", 5) 
+myTuple = ("Hasindu", 2, 3, "BSE", 5)
 
 for item in myTuple:
-    print(item) # tuple is an ordered collection of items, so the output will be in the order of the input.
+    # tuple is an ordered collection of items, so the output will be in the order of the input.
+    print(item)
--- 3. Loops/2. while loop.py
+++ 3. Loops/2. while loop.py
@@ -1,9 +1,9 @@
 # While Loop in Python is used to execute a block of statements repeatedly until a given condition is satisfied. And when the condition becomes false, the line immediately after the loop in the program is executed.
 
-x = [1, 2, 3, 4, 5] # List
+x = [1, 2, 3, 4, 5]  # List
 
 count = 0
 
-while count <= 10: # Loop will stop when count is greater than 10
+while count <= 10:  # Loop will stop when count is greater than 10
     print("Count is: ", count)
-    count += 1 
+    count += 1
