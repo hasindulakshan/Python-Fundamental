0456bb390369a614ce6a87e6c5626691c6c9266b
Hasindu Lakshan <hasindulakshan0040@gmail.com.com>
2024-09-01 09:20:23 +0530

    self parameter

--- OOP/Classas.py
+++ OOP/Classas.py
@@ -1,41 +1,35 @@
-## OOP is a programming paradigm that uses "objects" and "classes". It is used to structure a software program into simple, reusable pieces of code blueprints (usually called classes), which are used to create individual instances of objects.
+# OOP is a programming paradigm that uses "objects" and "classes". It is used to structure a software program into simple, reusable pieces of code blueprints (usually called classes), which are used to create individual instances of objects.
 
 # Define a class
 
-class Me(): # should be capitalized.
-    pass # pass is a keyword that does nothing. It is used when a statement is required syntactically but you do not want any command or code to execute.
+class Me():  # should be capitalized.
+    pass  # pass is a keyword that does nothing. It is used when a statement is required syntactically but you do not want any command or code to execute.
 
 # Create an object of the class
 
-me1 = Me() # me is an object of the class Me
 
-me1.name = "Hasindu"
+me1 = Me()  # me is an object of the class Me
+
+me1.name = "name"
 me1.age = 25
 
-print(me1.name, me1.age) 
+print(me1.name, me1.age)
 
 
 # Define a class with an attribute
 
-class Me():
-    
-    def __init__(self): # __init__ is a constructor. It is automatically called always when an object is created.
-        print("I am a constructor")
-        
-    name = "Hasindu"
-    age = 25
-    
+class Person():
+
     def set_name(self, name):
         self.name = name
-    
+
     def Run(self):
         print("I am running")
-        
+
     def Walk(self):
         print("I am walking")
 
-  
-Me1 = Me()
-Me1.set_name("Lakshan")
-print(Me1.name)
 
+person1 = Person()
+person1.set_name("pass name here")
+print(person1.name)
